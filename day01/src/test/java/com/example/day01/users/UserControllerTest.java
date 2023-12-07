14d6e3a8cf86969329cbb9bb52bb9f237c54d54d
up1 <somkiat.p@gmail.com>
2023-12-07 14:11:59 +0700

    User not found case

--- day01/src/test/java/com/example/day01/users/UserControllerTest.java
+++ day01/src/test/java/com/example/day01/users/UserControllerTest.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -33,5 +34,15 @@ class UserControllerTest {
         expected.setLast_name("pui");
         expected.setAge(40);
         assertEquals(expected, result);
+    }
+
+    @Test
+    @DisplayName("user not found case for get user by id =2")
+    void getUserById2() {
+        ResponseEntity<UserErrorResponse> result
+                = restTemplate.getForEntity("/users/2", UserErrorResponse.class);
+        assertEquals(200, result.getStatusCode().value());
+        assertEquals("User id=2 not found", result.getBody().getMessage());
+
     }
 }
