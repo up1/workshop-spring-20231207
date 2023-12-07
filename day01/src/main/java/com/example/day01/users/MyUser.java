567e801bbc14cd5f858a4913f372061da3c480aa
up1 <somkiat.p@gmail.com>
2023-12-07 15:06:02 +0700

    Add entity

---
+++ day01/src/main/java/com/example/day01/users/MyUser.java
@@ -0,0 +1,46 @@
+package com.example.day01.users;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="tbl_user")
+public class MyUser {
+    @Id @GeneratedValue
+    private int id;
+    @Column(name="fname", unique = true)
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
