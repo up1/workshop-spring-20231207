b9debe46934ab77a0ba5196c658b4af596d24cd9
up1 <somkiat.p@gmail.com>
2023-12-07 11:42:19 +0700

    Change test case name

--- day01/src/test/java/com/example/day01/HelloControllerTest.java
+++ day01/src/test/java/com/example/day01/HelloControllerTest.java
@@ -1,5 +1,6 @@
 package com.example.day01;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -15,6 +16,7 @@ class HelloControllerTest {
     private TestRestTemplate restTemplate;
 
     @Test
+    @DisplayName("ทำการทดสอบ hello api นะ ต้องได้ค่า hello spring boot")
     void sayHi() {
         HelloResponse result
                 = restTemplate.getForObject("/hello", HelloResponse.class);
