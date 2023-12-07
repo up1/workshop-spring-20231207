248d1a3ca860a260573586504260dcc2040ec7d4
up1 <somkiat.p@gmail.com>
2023-12-07 11:29:33 +0700

    Add spring boot testing

---
+++ day01/src/test/java/com/example/day01/HelloControllerTest.java
@@ -0,0 +1,22 @@
+package com.example.day01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+class HelloControllerTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    void sayHi() {
+        HelloResponse result = restTemplate.getForObject("/hello", HelloResponse.class);
+        assertEquals("Hello Spring Boot", result.getMessage());
+    }
+}
