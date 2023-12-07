48512116425c7b048f936c2a52e6539438527dcc
up1 <somkiat.p@gmail.com>
2023-12-07 16:11:39 +0700

    Initial data for test

--- day01/src/main/java/com/example/day01/Day01Application.java
+++ day01/src/main/java/com/example/day01/Day01Application.java
@@ -1,11 +1,27 @@
 package com.example.day01;
 
+import com.example.day01.users.MyUser;
+import com.example.day01.users.UserRepository;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class Day01Application {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@PostConstruct
+	public void initialDataForTest() {
+		MyUser user1 = new MyUser();
+		user1.setFirstName("somkiat");
+		user1.setLastName("pui");
+		user1.setAge(40);
+		userRepository.save(user1);
+	}
 
 	public static void main(String[] args) {
 		ConfigurableApplicationContext context
