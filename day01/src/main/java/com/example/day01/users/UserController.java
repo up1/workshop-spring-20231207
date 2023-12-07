d66cf25e296354436d6bf2da0dd500d771d6bf18
up1 <somkiat.p@gmail.com>
2023-12-07 12:12:47 +0700

    Custom statuc code for success case

--- day01/src/main/java/com/example/day01/users/UserController.java
+++ day01/src/main/java/com/example/day01/users/UserController.java
@@ -1,13 +1,16 @@
 package com.example.day01.users;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
 
     @GetMapping("/users/{id}")
+    @ResponseStatus(HttpStatus.OK)
     public UserResponse getUserById(@PathVariable int id) {
         UserResponse response = new UserResponse();
         response.setUserId(id);
