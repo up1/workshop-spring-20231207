ac868083f7a4e1fec189f3d5ca031de31705e820
up1 <somkiat.p@gmail.com>
2023-12-07 16:27:00 +0700

    Show sql and tracnsaction scope

--- day01/src/main/java/com/example/day01/users/UserService.java
+++ day01/src/main/java/com/example/day01/users/UserService.java
@@ -1,6 +1,7 @@
 package com.example.day01.users;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -13,11 +14,13 @@ public class UserService {
         this.userRepository = userRepository;
     }
 
+    @Transactional
     public UserResponse getById(int userId) {
         Optional<MyUser> user =  userRepository.findById(userId);
         if (user.isEmpty()) {
             throw new UserNotFoundException("User id=%d not found".formatted(userId));
         }
+
         UserResponse response = new UserResponse();
         response.setUser_id(userId);
         response.setFirst_name(user.get().getFirstName());
