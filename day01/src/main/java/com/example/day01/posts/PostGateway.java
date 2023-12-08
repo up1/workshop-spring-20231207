ca9dd6f7fdefda438c402bdc0f451d614d3a61df
up1 <somkiat.p@gmail.com>
2023-12-08 13:46:19 +0700

    Use rest template to call external service

--- day01/src/main/java/com/example/day01/posts/PostGateway.java
+++ day01/src/main/java/com/example/day01/posts/PostGateway.java
@@ -3,7 +3,10 @@ package com.example.day01.posts;
 import com.example.day01.Gateway;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Optional;
 
 @Gateway
 public class PostGateway {
@@ -11,8 +14,14 @@ public class PostGateway {
     @Autowired
     private RestTemplate restTemplate;
 
-    public PostResponse getById(int id) {
-        return null;
+    public Optional<PostResponse> getById(int id) {
+        String url = "https://jsonplaceholder.typicode.com/posts/" + id;
+        try {
+            PostResponse result = restTemplate.getForObject(url, PostResponse.class);
+            return Optional.of(result);
+        }catch (RestClientException e) {
+            return Optional.empty();
+        }
     }
 
 }
