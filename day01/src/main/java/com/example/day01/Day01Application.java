99a90f25866b6144c22e75a33c4154b449401fde
up1 <somkiat.p@gmail.com>
2023-12-08 11:12:28 +0700

    Demo with jdbc template

--- day01/src/main/java/com/example/day01/Day01Application.java
+++ day01/src/main/java/com/example/day01/Day01Application.java
@@ -1,15 +1,23 @@
 package com.example.day01;
 
+import com.example.day01.customers.Customer;
 import com.example.day01.users.MyUser;
 import com.example.day01.users.UserRepository;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @SpringBootApplication
-public class Day01Application {
+public class Day01Application implements CommandLineRunner {
 
 //	@Autowired
 //	private UserRepository userRepository;
@@ -23,9 +31,9 @@ public class Day01Application {
 //		userRepository.save(user1);
 //	}
 
-	public static void main(String[] args) {
-		ConfigurableApplicationContext context
-				= SpringApplication.run(Day01Application.class, args);
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context
+                = SpringApplication.run(Day01Application.class, args);
 
 //		UserResponse ur1 = (UserResponse)context.getBean("userResponse");
 //		ur1.setUser_id(1);
@@ -35,6 +43,32 @@ public class Day01Application {
 //
 //		System.out.println(ur1.getUser_id());
 //		System.out.println(ur2.getUser_id());
-	}
+    }
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    @Override
+    public void run(String... strings) throws Exception {
+
+        jdbcTemplate.execute("DROP TABLE IF EXISTS customers ");
+        jdbcTemplate.execute("CREATE TABLE customers(" +
+                "id SERIAL, first_name VARCHAR(255), last_name VARCHAR(255))");
+
+        // Split up the array of whole names into an array of first/last names
+        List<Object[]> splitUpNames = Stream.of("John Woo", "Jeff Dean", "Josh Bloch", "Josh Long")
+                .map(name -> name.split(" "))
+                .collect(Collectors.toList());
+
+        splitUpNames.forEach(
+                name -> System.out.printf("Inserting customer record for %s %s%n", name[0], name[1]));
+
+        jdbcTemplate.batchUpdate("INSERT INTO customers(first_name, last_name) VALUES (?,?)", splitUpNames);
+
+        jdbcTemplate.query(
+                        "SELECT id, first_name, last_name FROM customers WHERE first_name = ?",
+                        (rs, rowNum) -> new Customer(rs.getLong("id"), rs.getString("first_name"), rs.getString("last_name")), "Josh")
+                .forEach(customer -> System.out.println(customer.toString()));
+    }
 
 }
--- day01/src/main/java/com/example/day01/users/UserRepository.java
+++ day01/src/main/java/com/example/day01/users/UserRepository.java
@@ -1,6 +1,10 @@
 package com.example.day01.users;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface UserRepository extends JpaRepository<MyUser, Integer> {
+
 }
