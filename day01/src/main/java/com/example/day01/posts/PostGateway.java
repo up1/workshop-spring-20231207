e072176215045ece26f22bf7a898f6295092b0ce
up1 <somkiat.p@gmail.com>
2023-12-08 14:58:26 +0700

    Get url of service from properties file

--- day01/src/main/java/com/example/day01/posts/PostGateway.java
+++ day01/src/main/java/com/example/day01/posts/PostGateway.java
@@ -2,6 +2,7 @@ package com.example.day01.posts;
 
 import com.example.day01.Gateway;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
@@ -14,9 +15,11 @@ public class PostGateway {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Value("${external_url}")
+    private String externalApiUrl;
+
     public Optional<PostResponse> getById(int id) {
-//        String url = "https://jsonplaceholder.typicode.com/posts/" + id;
-        String url = "http://localhost:9999/posts/" + id;
+        String url = externalApiUrl + "/posts/" + id;
         try {
             PostResponse result = restTemplate.getForObject(url, PostResponse.class);
             return Optional.of(result);
