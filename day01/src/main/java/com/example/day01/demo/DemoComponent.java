9c811b1be986d363b30342755ed7b3568762b7ed
up1 <somkiat.p@gmail.com>
2023-12-08 10:16:39 +0700

    Copy resource folder from main to test

--- day01/src/main/java/com/example/day01/demo/DemoComponent.java
+++ day01/src/main/java/com/example/day01/demo/DemoComponent.java
@@ -4,10 +4,35 @@ package com.example.day01.demo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 @Component
 public class DemoComponent {
 
     @Autowired
     private MyService service2;
+
+    void todo() {
+        try{
+            System.out.println(1/0);
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    void db(){
+        PreparedStatement prepStmt = null;
+        ResultSet rs = null;
+        try(Connection conn = DriverManager.getConnection("")) {
+
+        } catch (Exception e) {
+
+        } finally {
+
+        }
+    }
 
 }
